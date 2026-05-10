3bc0e261f6df15b63a680de3e0e0999645d4b143
Axyl S <axylschulz0@gmail.com>
2026-05-10 00:59:48 -0600

    Show all hobbies without separate other hobbies card

--- src/hobbiesCards.tsx
+++ src/hobbiesCards.tsx
@@ -2,18 +2,10 @@ import { hobbies } from './hobbies';
 import type { CardItem } from './types';
 
 export const featuredHobbies: CardItem[] = [
-  ...hobbies
-    .filter((hobby) => hobby.section === 'featured')
-    .map((hobby) => ({
-      title: hobby.title,
-      href: `/hobbies/${hobby.slug}`,
-      description: hobby.description,
-      tags: hobby.tags
-    })),
-  {
-    title: 'Other Hobbies',
-    href: '/other_hobbies',
-    description: 'Explore additional hobbies and interests outside of software development.',
-    tags: ['Interests', 'Lifestyle', 'Personal Growth', 'Portfolio']
-  }
+  ...hobbies.map((hobby) => ({
+    title: hobby.title,
+    href: `/hobbies/${hobby.slug}`,
+    description: hobby.description,
+    tags: hobby.tags
+  }))
 ];
