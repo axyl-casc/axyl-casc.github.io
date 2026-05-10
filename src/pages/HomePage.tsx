52671af1871564a3e0b98c2b94a9c5038802e67d
Axyl S <axylschulz0@gmail.com>
2026-05-10 00:59:45 -0600

    Center core technology tags on home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -210,7 +210,7 @@ export function HomePage() {
 
       <div className="divider my-0">Core Technologies</div>
 
-      <div className="flex flex-wrap gap-2">
+      <div className="flex flex-wrap justify-center gap-2">
         {[
           'C++',
           'C',
